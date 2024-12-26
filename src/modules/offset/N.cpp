af17f46d8300d42f67f3c096723b2d0bf9c3545d
Shahriori <ShahTheShah@ya.ru>
2024-12-26 15:56:52 +0300

    add N

---
+++ src/modules/offset/N.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+    int first, second;
+
+    cin >> first >> second;
+
+    if(first > second)
+        cout << first << endl;
+    else
+        cout << second << endl;
+
+    return 0;
+}
